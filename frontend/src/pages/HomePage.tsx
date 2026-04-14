dafd7c4ac383a9bd3c2f9dcfafd7f49e0dd3c832
Zakkios <laskari.zakaria@gmail.com>
2026-04-14 21:16:20 +0200

    feat: temporary redesign of HomePage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,25 +1,46 @@
 import { useEffect, useState } from "react";
-import HelloApi from "../features/helloWorld/api";
+import MangaApi from "../features/manga/api";
+import type { MangaResponse } from "../features/manga/types";
 
 const HomePage = () => {
-  const [helloMessage, setHelloMessage] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [manga, setManga] = useState<MangaResponse | null>(null);
+
   useEffect(() => {
-    const fetchHelloMessage = async () => {
+    const fetchMangaData = async () => {
       try {
-        const response = await HelloApi();
-        setHelloMessage(response.message);
+        const response = await MangaApi();
+        setManga(response);
+        setLoading(false);
       } catch (error) {
-        console.error("Error fetching hello message:", error);
+        console.error("Error fetching manga data:", error);
+        setLoading(false);
       }
     };
 
-    fetchHelloMessage();
+    fetchMangaData();
   }, []);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl font-bold mb-4">Test API</h1>
-      <p className="text-lg">Reçu par l'API : {helloMessage}</p>
+      <p className="text-lg">Manga tiré au sort :</p>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <p className="mb-4">
+            Id : <strong>{manga?.id}</strong>
+          </p>
+          <p className="mb-4">
+            Titre : <strong>{manga?.title}</strong>
+          </p>
+          <p className="mb-4">
+            Auteur : <strong>{manga?.author}</strong>
+          </p>
+          <img src={manga?.coverUrl} alt="Cover" />
+        </>
+      )}
     </div>
   );
 };
