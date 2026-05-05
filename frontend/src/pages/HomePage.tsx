f628366307b1b57060dc4476f5e4ac6f18b9b702
Zakkios <laskari.zakaria@gmail.com>
2026-05-05 11:17:35 +0200

    feat(homepage): add infinite scroll for manga list
    - Create MangaPreviewCard component
    - Implement infinite scroll for manga
    - Fetch manga data by page
    - Display manga list using new component
    - Add navigation to manga detail page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,13 +1,69 @@
+import InfiniteScroll from "react-infinite-scroll-component";
 import { MangaSearch } from "../features/manga/components/MangaSearch";
+import { useState } from "react";
+import type { MangaShortResponse } from "../features/manga/types";
+import { getMangaByPage } from "../features/manga/api";
+import { MangaPreviewCard } from "../features/manga/components/MangaPreviewCard";
+import { useNavigate } from "react-router";
 
 const HomePage = () => {
+  const [page, setPage] = useState<number>(0);
+  const [items, setItems] = useState<MangaShortResponse[]>([]);
+  const [hasMore, setHasMore] = useState<boolean>(true);
+  const [loading, setLoading] = useState<boolean>(false);
+  const navigate = useNavigate();
+
+  const fetchMore = async () => {
+    if (loading || !hasMore) return;
+
+    setLoading(true);
+
+    const newItems = await getMangaByPage(page);
+
+    setItems((prevItems) => [...prevItems, ...newItems]);
+    setPage((prevPage) => prevPage + 1);
+
+    if (newItems.length < 20) {
+      setHasMore(false);
+    }
+
+    setLoading(false);
+  };
+
+  const handleClickPreviewCard = (id: string) => {
+    navigate(`/manga/${id}`);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-10 px-4">
-      <h1 className="text-4xl font-extrabold mb-8 bg-gradient-to-r from-blue-400 to-emerald-400 bg-clip-text text-transparent">
+      <h1 className="text-4xl font-extrabold mb-8 bg-linear-to-r from-blue-400 to-emerald-400 bg-clip-text text-transparent">
         LastSave Explorer
       </h1>
 
       <MangaSearch />
+      <InfiniteScroll
+        dataLength={items.length}
+        next={fetchMore}
+        hasMore={hasMore}
+        loader={<p className="text-zinc-400 text-center py-4">Chargement...</p>}
+        endMessage={
+          <p className="text-zinc-500 text-center py-4">
+            Tous les mangas sont chargés.
+          </p>
+        }
+      >
+        <div className="w-full max-w-2xl flex flex-col gap-3">
+          {items.map((item) => (
+            <button
+              className="cursor-pointer"
+              onClick={() => handleClickPreviewCard(item.id)}
+              key={item.id}
+            >
+              <MangaPreviewCard key={item.id} manga={item} />
+            </button>
+          ))}
+        </div>
+      </InfiniteScroll>
     </div>
   );
 };
