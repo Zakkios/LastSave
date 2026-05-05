dcadbc7965416e80875cd1ef23648a6c616498dd
Zakkios <laskari.zakaria@gmail.com>
2026-05-05 09:55:15 +0200

    refactor(manga-dto): Introduce MangaShortDTO
    - Create new DTO for short manga representation
    - Update MangaMapper to use MangaShortDTO
    - Replace MangaDTO usage in mapper

--- backend/src/DTO/MangaShortDTO.php
+++ backend/src/DTO/MangaShortDTO.php
@@ -2,7 +2,7 @@
 
 namespace App\DTO;
 
-class MangaDTO
+class MangaShortDTO
 {
     public function __construct(
         public string $id,
--- backend/src/Mapper/MangaMapper.php
+++ backend/src/Mapper/MangaMapper.php
@@ -2,29 +2,29 @@
 
 namespace App\Mapper;
 
-use App\DTO\MangaDTO;
+use App\DTO\MangaShortDTO;
 use App\Mapper\Resolver\MangaTitleResolver;
 
 class MangaMapper
 {
     function __construct(private MangaTitleResolver $mangaTitleResolver) {}
 
-    public function fromEntity(array $mangaData, ?string $author = null, ?string $coverUrl = null): MangaDTO
+    public function fromEntity(array $mangaData, ?string $author = null, ?string $coverUrl = null): MangaShortDTO
     {
-        $mangaDTO = new MangaDTO(
+        $mangaShortDTO = new MangaShortDTO(
             id: $mangaData['data']['id'],
             title: $this->mangaTitleResolver->resolveTitle($mangaData['data']['attributes']),
             author: $author ?? '',
             coverUrl: $coverUrl ?? ''
         );
-        return $mangaDTO;
+        return $mangaShortDTO;
     }
 
     public function fromCollection(array $mangaData, ?array $authors = null, ?array $coverUrls = null): array
     {
         $mangaDTOs = [];
         foreach ($mangaData['data'] as $index => $manga) {
-            $mangaDTOs[] = new MangaDTO(
+            $mangaDTOs[] = new MangaShortDTO(
                 id: $manga['id'],
                 title: $this->mangaTitleResolver->resolveTitle($manga['attributes']),
                 author: $authors[$index] ?? '',
